e70f715ef4a1d495d6f2ce6f8fcbbad062746457
unnatsharma <unnat.sharma2912@gmail.com>
2022-12-31 18:33:07 -0700

    initial commit

---
+++ learn-spring-framework/src/main/java/com/litecode/learnspringframework/AppGamingBasicJava.java
@@ -0,0 +1,9 @@
+package com.litecode.learnspringframework;
+
+public class AppGamingBasicJava {
+    public static void main(String[] args) {
+        var marioGame = new MarioGame();
+        var gameRunner = new GameRunner(marioGame);
+        gameRunner.run();
+    }
+}
